dc9adbf749acf47b996a55c2d865a455d4e721ae
Max Wolf <max.wolf91@web.de>
2024-05-29 00:32:47 +0200

    First commit - commiting all files to the repo

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css'
+
+const Page: React.FC = () => {
+    return (
+        <div className="container">
+            <h1 className="title">Willkommen, Max!</h1>
+            <p className="subtitle">Schön, dass du hier bist.</p>
+            <button className="button">Klicke mich an!</button>
+        </div>
+    );
+}
+
+export default Page;
